ee812922969925a437145250e780c0627092e56d
arseniyyy <arskosh05@mail.ru>
2023-10-25 17:20:48 +0700

    Minor changes

--- 8/4.py
+++ 8/4.py
@@ -3,13 +3,15 @@ from itertools import product
 
 
 def f():
-    alphabet = 'абвгдэюя'
+    alphabet = 'абвгд'
     comb = 'эюя'
     c = 0
-    for item in product(comb, alphabet, alphabet, alphabet, alphabet):
-        if any(char == item[-1] for char in comb):
-            if not any(char in item[1:-1] for char in comb):
-                c += 1
+    # for item in product(comb, alphabet, alphabet, alphabet, alphabet):
+    #     if any(char == item[-1] for char in comb):
+    #         if not any(char in item[1:-1] for char in comb):
+    #             c += 1
+    for t in product(comb, alphabet, alphabet, alphabet, comb):
+        c += 1
     return c
 
 
